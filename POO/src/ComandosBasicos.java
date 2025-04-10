344d7b7475815fbfd5643cf50932d9abd2ae9704
Amancjusz <luks.amancio@gmail.com>
2025-04-10 19:12:09 -0300

    Add files via upload

---
+++ POO/src/ComandosBasicos.java
@@ -0,0 +1,10 @@
+public class ComandosBasicos {
+    public static void main(String[] args) {
+        //Variaveis de tipos primitivos
+        boolean TrueOrFalse = true;
+        int numerosReais = 0 // 100000000 a -100000000
+        double decimais
+        System.out.println("Hello World");
+    }
+}
+
